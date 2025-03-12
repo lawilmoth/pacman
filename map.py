da9c8477803b74161bb99bb2fc21bf4b7ecb3b01
mrw <lwilmoth@eriesd.org>
2025-03-12 13:10:52 -0400

    added map and made sprites

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ import pygame
 from consumable import Consumable
 class Map:
     """A tool for building maps"""
-    GAP = 10
+    GAP = 50
     def __init__(self):
         self.pellets = [
             [0, 1, 1, 1, 0],
@@ -16,6 +16,6 @@ class Map:
         for i, row in enumerate(self.pellets):
             for j, item in enumerate(row):
                 print(f"({i}, {j}): {item}")
-                cons = Consumable(game, i, j, 5, (255, 255, 255))
+                cons = Consumable(game, i*self.GAP, j*self.GAP, 5, (255, 255, 255))
                 game.consumables.add(cons)
 
